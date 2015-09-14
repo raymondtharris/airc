520a4c285ef74a8dda232c71978c874c789c4832
Tim Harris <raymondtimothyharris@gmail.com>
2015-09-13 17:24:43 -0700

    Initial project creation commit.

---
+++ aIRC/aIRC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  aIRC
+//
+//  Created by Tim Harris on 9/13/15.
+//  Copyright © 2015 Tim Harris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
